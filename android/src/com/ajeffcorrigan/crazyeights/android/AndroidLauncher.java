7c66139b61259d63b768ce6332fa2cb4eebb13e6
Jeff Corrigan <ajeffcorrigan@yahoo.com>
2015-01-20 13:33:51 -0400

    Initial Commit

---
+++ android/src/com/ajeffcorrigan/crazyeights/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.ajeffcorrigan.crazyeights.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ajeffcorrigan.crazyeights.CrazyEights;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CrazyEights(), config);
+	}
+}
---
+++ desktop/src/com/ajeffcorrigan/crazyeights/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ajeffcorrigan.crazyeights.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ajeffcorrigan.crazyeights.CrazyEights;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CrazyEights(), config);
+	}
+}
